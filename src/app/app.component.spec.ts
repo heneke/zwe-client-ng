737354dd69c54e499d2cbbc2346a8d7b7940c888
Hendrik Heneke <hendrik@pixelwunder.net>
2017-09-03 08:41:30 +0200

    Added loading bar.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,26 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async} from '@angular/core/testing';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {AppRoutingModule} from './app-routing.module';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+            imports: [
+                SharedModule,
+                AppRoutingModule,
+            ]
+        }).compileComponents();
+    }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 
 });
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
 
 @NgModule({
     imports: [
         CommonModule,
         NgbModule.forRoot(),
+        SlimLoadingBarModule.forRoot(),
     ],
     declarations: [
     ],
     exports: [
         CommonModule,
-        NgbModule
+        NgbModule,
+        SlimLoadingBarModule,
     ],
 })
 export class SharedModule {
